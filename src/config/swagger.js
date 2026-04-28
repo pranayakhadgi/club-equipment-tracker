f8d15187fb732c44ac186fec5fb870e2ef777576
Pranaya Khadgi Shahi <pranaya.khadgi99@gmail.com>
2026-04-28 09:15:08 -0500

    fixed the api endpoint discrepancy

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,31 +1,10 @@
-const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-
-const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: "Equipment Inventory API",
-            version: "1.0.0",
-            description: "Campus equipment management system with ghost return detection",
-            contact: {
-                name: "[theMFdev]",
-                email: "an.al123@gmail.com"
-            }
-        },
-        servers: [
-            {
-                url: "http://localhost:3000",
-                description: 'Development server'
-            }
-        ]
-    },
-    apis: ['./src/routes/*.js']
-};
-
-const specs = swaggerJsdoc(options);
+const swaggerDocs = require('./swagger-docs');
 
 module.exports = {
     serve: swaggerUi.serve,
-    setup: swaggerUi.setup(specs)
+    setup: (req, res, next) => {
+        const swaggerDocs = require('./swagger-docs');
+        swaggerUi.setup(swaggerDocs)(req, res, next);
+    }
 };
