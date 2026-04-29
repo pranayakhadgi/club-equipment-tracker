e9c9ef2a2b4f4e0f50922cf0e92e0bf18b5f8a28
Pranaya Khadgi Shahi <pranaya.khadgi99@gmail.com>
2026-04-29 12:39:32 -0500

    format staged

--- src/config/database.js
+++ src/config/database.js
@@ -1,20 +1,20 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-connectionString:
-process.env.DATABASE_URL,
+    connectionString:
+        process.env.DATABASE_URL,
 });
 
 //error handling within startup connection (test)
-pool.query('SELECT NOW()', (err, res) => 
-{
-if(err) {
-console.error('Database connection failed:', err);
-} else {
-console.log('Database connected at:', res.rows[0].now);}
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error('Database connection failed:', err);
+    } else {
+        console.log('Database connected at:', res.rows[0].now);
+    }
 });
 
 module.exports = {
-query: (text, params) => pool.query(text, params), 
+    query: (text, params) => pool.query(text, params),
 };
 
